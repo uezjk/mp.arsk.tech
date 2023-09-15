028964453835c8e8c0c780fc2ce9463359e90466
刘珂珂 <ifme.in@gmail.com>
2023-09-15 11:18:54 +0800

    clean files

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ type Props = { style: "dark" | "light"; siteName: string; children?: unknown };
 export function Header(props: Props) {
   return (
     <div class="bg-gray-200 p-4">
-      <a href="/mp/broadcast">
+      <a href="/x/broadcast">
         <img class="mx-auto block h-8" src="/images/title-broadcast.png" alt="热点播报站" />
       </a>
     </div>
--- src/islands/BroadcastCard.tsx
+++ src/islands/BroadcastCard.tsx
@@ -5,10 +5,10 @@ export default function BroadcastCard(props: Props) {
   return (
     <div class="md:flex">
       <div class="md:shrink-0">
-        <a href={"/mp/broadcast/" + item.id}><img class="h-36 w-full object-cover md:w-96" src={item.thumb} /></a>
+        <a href={"/x/broadcast/" + item.id}><img class="h-36 w-full object-cover md:w-96" src={item.thumb} /></a>
       </div>
       <div class="p-4 bg-gray-100 w-full">
-        <a href={"/mp/broadcast/" + item.id} class="tracking-wide text-indigo-500">{item.title}</a>
+        <a href={"/x/broadcast/" + item.id} class="tracking-wide text-indigo-500">{item.title}</a>
         <div class="text-gray-400 flex gap-2 text-sm pt-2">
           <span>发布日期 {item.publish}</span>
           <span>浏览 {item.views}</span>
