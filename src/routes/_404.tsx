fa15d453a2c00b4decbc4f6659f59fd967f1db02
刘珂珂 <ifme.in@gmail.com>
2023-09-15 17:20:17 +0800

    add brandlogo svg

--- src/routes/_404.tsx
+++ src/routes/_404.tsx
@@ -1,4 +1,5 @@
 import { Head } from "$fresh/runtime.ts";
+import BrandLogo from "~/components/BrandLogo.tsx";
 
 export default function Error404() {
   return (
@@ -10,8 +11,8 @@ export default function Error404() {
         class="h-screen flex items-center bg[#000851]"
         style="background: linear-gradient(45deg, #1CB5E0 0%, #000851 100%);"
       >
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img class="h-12" src="/images/title-broadcast.png" alt="Logo" />
+        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center text-white font-light">
+          <BrandLogo className="h-14" />
           <h1 class="text-4xl font-bold pt-10">404 - NOT FOUND</h1>
           <div class="my-4">您要访问的页面不存在</div>
           <a href="/" class="border-b">返回首页</a>
