c36599119c9c215d8e56124aabb3d241e206b85c
刘珂珂 <ifme.in@gmail.com>
2023-09-26 15:48:14 +0800

    utm测试

--- src/components/GlobalFooter.tsx
+++ src/components/GlobalFooter.tsx
@@ -1,7 +1,7 @@
 export default function () {
   return (
     <footer class="mt-auto mx-auto flex gap-4 text-sm">
-      <a href="https://www.energytrust.com.cn" target="_blank" class="text-gray-400 border-b border-dashed">
+      <a href="https://www.energytrust.com.cn/?utm_source=mp.arsk.tech" target="_blank" class="text-gray-400 border-b border-dashed">
         &copy; {new Date().getFullYear()} 安瑞信杰数字科技
       </a>
     </footer>
