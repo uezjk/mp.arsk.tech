83c50b91a6b9f761482bb2c6dbfaadde215b85ed
刘珂珂 <ifme.in@gmail.com>
2023-10-12 15:32:40 +0800

    fix localStorage in deno deploy

--- src/utils/state/auth.ts
+++ src/utils/state/auth.ts
@@ -1,21 +1,36 @@
 import { computed, signal } from "@preact/signals";
 
+const storage = {
+  removeItem: (name: string) => {
+    if (typeof window === "undefined") return;
+    window.localStorage.removeItem(name);
+  },
+  setItem: (name: string, value: string) => {
+    if (typeof window === "undefined") return;
+    window.localStorage.setItem(name, value);
+  },
+  getItem: (name: string) => {
+    if (typeof window === "undefined") return null;
+    return window.localStorage.getItem(name);
+  },
+};
+
 export function useAuth() {
   const user = signal<null | { id: string; email: string }>(null);
   const token = signal<string>("");
   const isAuthenticated = computed(() => token.value !== "");
 
   const destroy = () => {
-    localStorage.removeItem("auth:session");
+    storage.removeItem("auth:session");
     token.value = "";
     user.value = null;
   };
 
   const save = (value: string) => {
-    localStorage.setItem("auth:session", value);
+    storage.setItem("auth:session", value);
   };
 
-  const value = localStorage.getItem("auth:session");
+  const value = storage.getItem("auth:session");
   if (value) {
     try {
       const auth = JSON.parse(value);
